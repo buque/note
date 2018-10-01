975430f0c27ee0556b87410f7a7970d2e802de76
max <285525988@qq.com>
2018-10-01 20:32:25 +0800

    modified:   .vscode/launch.json 	new file:   0.png 	new file:   1.png 	new file:   10.png 	new file:   11.png 	new file:   12.png 	new file:   128.png 	new file:   129.png 	new file:   13.png 	new file:   130.png 	new file:   131.png 	new file:   132.png 	new file:   134.png 	new file:   135.png 	new file:   136.png 	new file:   14.png 	new file:   15.png 	new file:   2 (1).png 	new file:   2.png 	new file:   3.png 	new file:   4.png 	new file:   5.png 	new file:   6.png 	new file:   7.png 	new file:   8.png 	new file:   9.png 	modified:   main.py 	modified:   note.ui 	modified:   ui_note.py

--- ui_note.py
+++ ui_note.py
@@ -6,22 +6,45 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(233, 352)
-        self.listView = QtWidgets.QListView(Form)
-        self.listView.setEnabled(True)
-        self.listView.setGeometry(QtCore.QRect(0, 0, 231, 351))
-        self.listView.setObjectName("listView")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
 
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "便签"))
 
+class NoteWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedHeight(300)
+        self.setFixedWidth(250)
+        self.setupNote()
+        self.show()
+
+    def setupNote(self):
+        self.table = self.initTable()
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.table)
+        self.setLayout(hbox)
+        self.setCentralWidget(self.table)
+
+    def initTable(self):
+        table = QTableWidget(5, 3)
+        table.setWindowTitle("biaoge")
+        table.setWindowIcon(QIcon("2.png"))
+        
+        columName = ['优先级', '内容', '状态']
+        table.setHorizontalHeaderLabels(columName)
+        self.setTableInitData(table)
+        table.setAlternatingRowColors(True)#交替使用颜色
+        table.setShowGrid(False)#表格线隐藏
+        #table.verticalHeader().setFrameShadow()
+        #table.setFrameShadow(table, QFrame.NoFrame)#边框隐藏
+
+        return table
+    
+    def setTableInitData(self, table):
+        for i in range(5):
+            for j in range(3):
+                table.setItem(i, j, QTableWidgetItem(str(i)+str(j)))
